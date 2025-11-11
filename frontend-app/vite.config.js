8300f5c9dd271bd07ae201d19a6c202fc686ec6e
Gariss <>
2025-11-11 06:35:50 +0300

    modified https config, cors config

--- frontend-app/vite.config.js
+++ frontend-app/vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    https: {
+      key: fs.readFileSync('/etc/ssl/frontend/frontend.key'),
+      cert: fs.readFileSync('/etc/ssl/frontend/frontend.crt')
+    },
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL,
+        target: 'https://localhost:5092',
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '') // Убираем /api из пути
       }
     }
   }
